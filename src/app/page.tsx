cdd165a3cfc1f3c9e4135bf03311e2e7bf5e7289
dhir4j <djkapse007@gmail.com>
2025-07-28 21:11:26 +0000

    i said reduce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -259,7 +259,7 @@ export default function Home() {
                       <Link href="/about-us">Learn More About Us</Link>
                   </Button>
               </div>
-               <div className="md:col-span-4 relative h-[450px] w-full mx-auto">
+               <div className="md:col-span-4 relative h-[400px] w-full mx-auto">
                   <Image 
                     src="/images/shipment_single_person_standing.jpg"
                     alt="Our team at work"
